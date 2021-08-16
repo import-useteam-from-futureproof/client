c3e61d13828760fa6a06de84549ce84889a9b1c6
Velo <hartley.max@outlook.com>
2021-08-16 15:49:19 +0100

    stub leaderboard layout component

--- src/layout/Leaderboard/index.js
+++ src/layout/Leaderboard/index.js
@@ -1,5 +1,52 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default () => {
-	return <ul aria-label="leaderboard"></ul>;
+	const [leaderboardData, setLeaderboardData] = useState([]);
+	const [isError, setIsError] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const BASE_URL = 'http://localhost:5000';
+
+	useEffect(() => {
+		const fetchLeaderboardData = async () => {
+			try {
+				setIsError(false);
+				setIsLoading(true);
+				const { data } = await axios.get(`${BASE_URL}/highscores`);
+				setLeaderboardData(data);
+				setIsLoading(false);
+			} catch (err) {
+				console.error(err);
+				setIsLoading(false);
+				setIsError(true);
+			}
+		};
+
+		fetchLeaderboardData();
+	}, []);
+
+	const renderLeaderboardData = () =>
+		leaderboardData.map(({ username, score }, i) => (
+			<li>
+				<span>{username}</span>
+				<span>{score}</span>
+				{i === leaderboardData.length - 1 ? null : <hr />}
+			</li>
+		));
+
+	return (
+		<ul aria-label="leaderboard">
+			{isLoading ? (
+				'Loading...'
+			) : isError ? (
+				<p>
+					Oops! Something went wrong (ノಠ益ಠ)ノ彡┻━┻
+					<br /> Please refresh or try again later.
+				</p>
+			) : (
+				renderLeaderboardData()
+			)}
+		</ul>
+	);
 };
