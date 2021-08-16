e34ecef527018d23837429f8b5156d10431483a3
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-08-16 12:27:56 +0100

    working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { Profile } from './pages';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { Profile } from "./pages";
 
 // Pages //
-import { LandingPage } from './pages';
+import { LandingPage } from "./pages";
 
 export default function App() {
-	return (
-		<>
-			<Switch>
-				<Route path="/profile">
-					<Profile />
-				</Route>
-				<Route path="/">
-					<LandingPage />
-				</Route>
-			</Switch>
-		</>
-	);
+  return (
+    <>
+      <Switch>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/">
+          <LandingPage />
+        </Route>
+      </Switch>
+    </>
+  );
 }
