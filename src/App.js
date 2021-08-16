c2894f612fdcd3cb4e044df7986ec2641a4eee93
Velo <hartley.max@outlook.com>
2021-08-16 14:24:54 +0100

    stub all pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Profile } from './pages';
-
-// Pages //
-import { LandingPage } from './pages';
+import { Profile, LandingPage, Leaderboard, Dashboard, Quiz, NotFound } from './pages';
 
 export default function App() {
 	return (
 		<>
 			<Switch>
-				<Route path="/profile">
+				<Route path='/profile'>
 					<Profile />
 				</Route>
-				<Route path="/">
+				<Route path='/leaderboard'>
+					<Leaderboard />
+				</Route>
+				<Route path='/dashboard'>
+					<Dashboard />
+				</Route>
+				<Route path='/quiz/:id'>
+					<Quiz />
+				</Route>
+				<Route path='/profile'>
+					<Profile />
+				</Route>
+				<Route exact path='/'>
 					<LandingPage />
+				</Route>
+				<Route path='/'>
+					<NotFound />
 				</Route>
 			</Switch>
 		</>
