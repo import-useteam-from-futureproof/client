6e93ce9ab6b75bbda623454045793005cfa1c46f
Velo <hartley.max@outlook.com>
2021-08-17 10:21:37 +0100

    asbtract auth redirects to component

--- src/pages/landingPage/index.js
+++ src/pages/landingPage/index.js
@@ -2,18 +2,8 @@ import React, { useEffect } from 'react';
 import styles from './style.module.css';
 import { About, Review } from '../../components';
 import { SignUpLogin } from '../../layout';
-import { useAuth } from '../../contexts/AuthContext';
-import { useHistory } from 'react-router-dom';
 
 const LandingPage = () => {
-	const { currentUser } = useAuth();
-	const { push } = useHistory();
-
-	useEffect(() => {
-		if (currentUser) {
-			push('/dashboard');
-		}
-	}, [currentUser]);
 	return (
 		<>
 			<SignUpLogin />
