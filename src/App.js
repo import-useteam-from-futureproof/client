b5638731893715a3d60eece574e626c5e816695e
Velo <hartley.max@outlook.com>
2021-08-17 16:33:17 +0100

    abstract room and quiz data to context provider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
+import { QuizProvider } from './contexts/QuizContext';
 import { RedirectUser } from './components';
 import { Profile, LandingPage, Leaderboard, Lobby, Quiz, NotFound, GDPR } from './pages';
 
@@ -17,12 +18,14 @@ export default function App() {
 				<Route path="/leaderboard">
 					<Leaderboard />
 				</Route>
-				<Route path="/lobby">
-					<Lobby />
-				</Route>
-				<Route path="/quiz/:id">
-					<Quiz />
-				</Route>
+				<QuizProvider>
+					<Route path="/lobby">
+						<Lobby />
+					</Route>
+					<Route path="/quiz/:id">
+						<Quiz />
+					</Route>
+				</QuizProvider>
 				<Route path="/profile">
 					<Profile />
 				</Route>
--- src/layout/NewQuiz/index.js
+++ src/layout/NewQuiz/index.js
@@ -1,27 +1,21 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import { useAuth } from '../../contexts/AuthContext';
+import { useQuiz } from '../../contexts/QuizContext';
 import { CreateRoomForm, QuizDetailForm } from '../../components';
 import { useHistory } from 'react-router-dom';
 
 export default () => {
+	const { createRoom, createQuiz } = useQuiz();
 	const { push } = useHistory();
 	const { currentUser } = useAuth();
 	const [roomData, setRoomData] = useState({});
 	const [quizData, setQuizData] = useState({});
-	const [showRoomForm, setShowRoomForm] = useState(false);
+	const [showRoomForm, setShowRoomForm] = useState(true);
 
 	const handleCreateRoomSubmit = async (roomFormData) => {
 		try {
 			setRoomData(roomFormData);
-			const { data } = await axios.post(`${process.env.BASE_URL}/rooms`, {
-				name: roomData.roomName,
-				owner: currentUser.uid,
-				max_room_size: roomData.maxPlayers,
-				public_room: roomData.public,
-				entry_pass: roomData.public ? null : roomData.password,
-			});
-			setRoomData(data);
+			await createRoom({ ...roomData, ownerId: currentUser.uid });
 			setShowRoomForm(false);
 		} catch (err) {
 			console.error(err);
@@ -31,12 +25,7 @@ export default () => {
 	const handleCreateQuizSubmit = async (quizFormData) => {
 		try {
 			setQuizData(quizFormData);
-			await axios.post(`${process.env.BASE_URL}/quiz`, {
-				room_id: quizData.room_id,
-				category: quizData.category,
-				difficulty: quizData.difficulty,
-				num_questions: quizData.numberOfQuestions,
-			});
+			await createQuiz(quizData);
 			push(`/quiz/${roomData.room_id}`);
 		} catch (err) {
 			console.error(err);
