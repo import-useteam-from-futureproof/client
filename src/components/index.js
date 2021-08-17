da361f9268d96060b607af8f00072f497190df66
Velo <hartley.max@outlook.com>
2021-08-17 17:12:35 +0100

    fetch all categories from api

--- src/components/index.js
+++ src/components/index.js
@@ -8,5 +8,5 @@ export { default as Review } from './Review';
 export { default as RedirectUser } from './RedirectUser';
 export { default as Footer } from './Footer';
 export { default as ErrorModal } from './ErrorModal';
-export { default as QuizDetailForm } from './QuizDetailForm';
+export { default as CreateQuizForm } from './CreateQuizForm';
 export { default as CreateRoomForm } from './CreateRoomForm';
