69b56764afe01d89766bc6df1e9ce66a56cc26eb
Zak Gogi <zakgogi1@gmail.com>
2021-08-18 21:53:02 +0100

    Began managing changing quiz components, and began work on socket making everyone change component

--- src/layout/QuizController/index.js
+++ src/layout/QuizController/index.js
@@ -1,20 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { LobbyWaitingRoom, Game, Results } from '../../components';
+import io from 'socket.io-client';
+// import { useQuiz } from '../../contexts/QuizContext';
+// import { useAuth } from '../../contexts/AuthContext';
 
 const QuizController = () => {
+	const [component, setComponent] = useState('WaitingRoom');
+	// const { roomData } = useQuiz();
+	// const { currentUser } = useAuth();
+	useEffect(() => {
+		const socket = io('https://pursuit-of-trivia.herokuapp.com/');
+		// socket.emit('joinRoom', { roomName: roomData.id, username: currentUser.displayName });
+		socket.on('advanceGame', (data) => {
+			updateComponent(data.component);
+		});
+	}, []);
+
+	const updateComponent = (component) => {
+		console.log('Received something from socket');
+		setComponent(component);
+	};
+
 	const handleGameEnd = (answers) => {
 		console.log(answers);
 	};
 
-	const handleHostStart = () => {
-		console.log('Quiz Started');
+	const handleHostStart = (roomId) => {
+		console.log(`Quiz Started in room ${roomId}`);
+		// Emit something that tells all components to move to Game
+		const socket = io('https://pursuit-of-trivia.herokuapp.com/');
+		socket.emit('advanceGame', { component: 'Game', roomName: roomId });
+	};
+
+	const componentToLoad = () => {
+		switch (component) {
+			case 'WaitingRoom':
+				return <LobbyWaitingRoom hostStartedQuiz={handleHostStart} />;
+			case 'Game':
+				return <Game onGameEnd={handleGameEnd} />;
+			case 'Results':
+				return <Results />;
+			default:
+				return <h1>Loading component...</h1>;
+		}
 	};
 
 	return (
 		<>
-			<Game onGameEnd={handleGameEnd} />
+			{/* <Game onGameEnd={handleGameEnd} />
 			<LobbyWaitingRoom hostStartedQuiz={handleHostStart} />
-			<Results />
+			<Results /> */}
+			{componentToLoad()}
 		</>
 	);
 };
