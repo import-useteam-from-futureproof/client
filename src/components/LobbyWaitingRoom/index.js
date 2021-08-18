8152cd0c7e411d2db25bffb02c1508ab21305bda
Zak Gogi <zakgogi1@gmail.com>
2021-08-18 16:23:28 +0100

    Initial stub for rendering a different waiting room depending on if you are the host in place

--- src/components/LobbyWaitingRoom/index.js
+++ src/components/LobbyWaitingRoom/index.js
@@ -3,20 +3,34 @@ import { useQuiz } from '../../contexts/QuizContext';
 import { useAuth } from '../../contexts/AuthContext';
 
 const LobbyWaitingRoom = () => {
-	// const [ setQuizUser] = useState('participant');
+	const [host, setHostBool] = useState(false);
 	const { roomData } = useQuiz();
 	const { currentUser } = useAuth();
 
-	useEffect(() => {});
-	console.log(roomData);
-	console.log(currentUser.uid);
-	return (
-		<>
+	useEffect(() => {
+		if (roomData.owner === currentUser.uid) {
+			setHostBool(true);
+		}
+	}, []);
+
+	const renderHostPage = () => {
+		return (
+			<form>
+				<h1>You are the host</h1>
+				<input type="submit" value="Start the quiz!"></input>
+			</form>
+		);
+	};
+
+	const renderParticipantPage = () => {
+		return (
 			<div>
-				<h1>Waiting...</h1>
+				<h1>Waiting for the host to start the game..</h1>
 			</div>
-		</>
-	);
+		);
+	};
+
+	return <>{host ? renderHostPage() : renderParticipantPage()}</>;
 };
 
 export default LobbyWaitingRoom;
