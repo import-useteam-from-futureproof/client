58db1d750594e5dc9a9a074e97a25d00f815bd7c
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-08-18 18:34:10 +0100

    Add Position key & styling

--- src/pages/Leaderboard/index.js
+++ src/pages/Leaderboard/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { NavBar } from '../../components';
 import { Leaderboard } from '../../layout';
+import styles from './style.module.css';
 
 export default () => {
 	return (
 		<>
 			<NavBar />
 			<main>
-				<h1>High Scores</h1>
+				<h1 className={styles.welcomeHeader}>High Scores</h1>
 				<Leaderboard />
 			</main>
 		</>
