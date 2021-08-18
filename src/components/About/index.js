ae38a4f1bbf686fa78084be184ad655fa1fc2625
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-08-18 11:27:34 +0100

    Finish About component styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,13 +4,11 @@ import styles from './style.module.css';
 const About = () => {
 	return (
 		<section className={styles.aboutMeSection}>
-			<img
-				className={styles.images}
-				src="https://www.nomadfoods.com/wp-content/uploads/2018/08/placeholder-1-e1533569576673.png"
-			></img>
+			<div className={styles.imageContainer}></div>
+
 			<section className={styles.aboutMeSectionText}>
-				<h2>Love to quiz?</h2>
-				<h3>of course you do...</h3>
+				<h2>Test your knowledge against your friends & the world.</h2>
+				<h3>...we know you’ve got what it takes.</h3>
 			</section>
 		</section>
 	);
