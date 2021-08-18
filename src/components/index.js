42fc2a063d1457d8ec32969dad846754559eb801
Zak Gogi <zakgogi1@gmail.com>
2021-08-18 15:51:44 +0100

    Stub quiz controller layout component

--- src/components/index.js
+++ src/components/index.js
@@ -14,3 +14,6 @@ export { default as GDPRNotice } from './GDPRNotice';
 export { default as Chatroom } from './Chatroom';
 export { default as ChatLog } from './ChatLog';
 export { default as FeaturesList } from './FeaturesList';
+export { default as LobbyWaitingRoom } from './LobbyWaitingRoom';
+export { default as Game } from './Game';
+export { default as Results } from './Results';
