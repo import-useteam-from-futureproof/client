03e22b3c11d91eeb900917ebcfe70a8ed2ac5e55
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-08-19 11:15:20 +0100

    Fix error feedback on login form

--- src/components/loginForm/index.js
+++ src/components/loginForm/index.js
@@ -5,6 +5,7 @@ import styles from './style.module.css';
 const LoginForm = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const [error, setError] = useState('');
 
 	const { login } = useAuth();
 
@@ -19,9 +20,28 @@ const LoginForm = () => {
 		try {
 			e.preventDefault();
 			const response = await login(email, password);
+			console.log(response);
 		} catch (err) {
+			renderErrors(err);
 			console.log(err);
 		}
+	};
+
+	const renderErrors = (error) => {
+		let displayedError;
+		switch (error.code) {
+			case 'auth/user-not-found':
+				displayedError = 'User not found.';
+				break;
+			case 'auth/wrong-password':
+				displayedError = 'Password Incorrect.';
+				break;
+
+			default:
+				displayedError = 'Login error. Please try again.';
+				break;
+		}
+		setError(displayedError);
 	};
 
 	return (
@@ -48,6 +68,7 @@ const LoginForm = () => {
 					/>
 				</label>
 				<input className={styles.loginButton} type="submit" value="log in" />
+				<p className={styles.errorDisplay}>{error}</p>
 			</form>
 			<hr className={styles.rule} />
 		</>
