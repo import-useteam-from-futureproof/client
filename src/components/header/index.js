ff87c275c697f85aa4aeeb72277ae3c49ca0512a
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-08-19 17:39:39 +0100

    Image fix

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import styles from './style.module.css';
+import test from '../../assets/logo.svg';
 
 const Header = () => {
 	return (
 		<section className={styles.headerContainer} aria-label="header container">
-			<img
-				aria-label="site logo"
-				className={styles.headerImage}
-				src={require('../../assets/logo.svg').default}
-			></img>
+			<img aria-label="site logo" className={styles.headerImage} src={test}></img>
 		</section>
 	);
 };
