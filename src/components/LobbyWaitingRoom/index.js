72752152d24c33f2fa9e8002fa69aaf3d6527eed
Zak Gogi <zakgogi1@gmail.com>
2021-08-19 19:08:53 +0100

    Set interval to check how many people are in the room every 5 seconds

--- src/components/LobbyWaitingRoom/index.js
+++ src/components/LobbyWaitingRoom/index.js
@@ -3,10 +3,13 @@ import { useQuiz } from '../../contexts/QuizContext';
 import { useAuth } from '../../contexts/AuthContext';
 import { useParams } from 'react-router-dom';
 import styles from './style.module.css';
-
-const LobbyWaitingRoom = ({ hostStartedQuiz, numPlayers }) => {
+import axios from 'axios';
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+const LobbyWaitingRoom = ({ hostStartedQuiz }) => {
+	const { id } = useParams();
 	const [host, setHostBool] = useState(false);
 	const [loading, setLoading] = useState(true);
+	const [numPlayers, setNumPlayers] = useState(1);
 	const { roomData } = useQuiz();
 	const { quizData } = useQuiz();
 	const { currentUser } = useAuth();
@@ -21,6 +24,20 @@ const LobbyWaitingRoom = ({ hostStartedQuiz, numPlayers }) => {
 		}
 	}, [quizData, roomData]);
 
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const { data } = await axios.get(`${BASE_URL}/rooms/${id}`);
+				setNumPlayers(data.participants.length);
+			} catch (err) {
+				console.log(err);
+			}
+		};
+
+		const intervalId = setInterval(fetchData, 5000);
+		return () => clearInterval(intervalId);
+	}, []);
+
 	const quizStart = (e) => {
 		e.preventDefault();
 		hostStartedQuiz(roomData.id);
@@ -34,6 +51,7 @@ const LobbyWaitingRoom = ({ hostStartedQuiz, numPlayers }) => {
 		return (
 			<form onSubmit={quizStart}>
 				<h1>You are the host</h1>
+				<h2>Room name: {roomData.name}</h2>
 				<h2>Category: {quizData.category}</h2>
 				<h2>Difficulty: {quizData.difficulty}</h2>
 				<h2>Number of questions: {quizData.questions.length}</h2>
@@ -47,6 +65,7 @@ const LobbyWaitingRoom = ({ hostStartedQuiz, numPlayers }) => {
 		return (
 			<>
 				<h1>Waiting for the host to start the game..</h1>
+				<h2>Room name: {roomData.name}</h2>
 				<h2>Category: {quizData.category}</h2>
 				<h2>Difficulty: {quizData.difficulty}</h2>
 				<h2>Number of questions: {quizData.questions.length}</h2>
