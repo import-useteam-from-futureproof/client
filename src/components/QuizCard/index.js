5bff3f34dd6c830d959c42183274a51182fdac0a
Velo <hartley.max@outlook.com>
2021-08-19 22:46:34 +0100

    stlye quiz questions

--- src/components/QuizCard/index.js
+++ src/components/QuizCard/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import styles from './style.module.css';
 
 export default ({ answers, question, time, handleClick }) => {
 	const renderAnswers = () =>
 		answers.map((answer, i) => (
-			<button dangerouslySetInnerHTML={{ __html: answer }} onClick={handleClick} key={i}></button>
+			<button
+				className={styles.button}
+				dangerouslySetInnerHTML={{ __html: answer }}
+				onClick={handleClick}
+				key={i}
+			></button>
 		));
 
 	return (
-		<div>
-			<p dangerouslySetInnerHTML={{ __html: question }}></p>
-			{renderAnswers()}
-			<p>Time left: {time}s</p>
+		<div className={styles.container}>
+			<h1>Question</h1>
+			<p className={styles.question} dangerouslySetInnerHTML={{ __html: question }}></p>
+			<h2>Answers</h2>
+			<div className={styles.buttons}>{renderAnswers()}</div>
+			<p className={styles.timer}>{time}s</p>
 		</div>
 	);
 };
