a5c1a19367420d3e7d81f8035ed4a3e3bfd359f6
Velo <hartley.max@outlook.com>
2021-08-19 10:58:24 +0100

    add functionality to handle game end function

--- src/layout/QuizController/index.js
+++ src/layout/QuizController/index.js
@@ -1,16 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { LobbyWaitingRoom, Game, Results } from '../../components';
+import { useParams } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
 
 const QuizController = ({ socket }) => {
+	const { id } = useParams();
+	const { currentUser } = useAuth();
 	const [component, setComponent] = useState('WaitingRoom');
 	useEffect(() => {
 		socket.on('advanceGame', (component) => {
 			setComponent(component);
 		});
+		socket.on('userFinished', (data) => {
+			console.log(data);
+		});
 	}, []);
 
 	const handleGameEnd = (answers) => {
-		console.log(answers);
+		const data = {
+			score: answers.reduce((acc, curr) => {
+				return acc + curr.score;
+			}, 0),
+			roomName: id,
+			userId: currentUser.uid,
+		};
+		socket.emit('userFinished', data);
 	};
 
 	const handleHostStart = (roomId) => {
