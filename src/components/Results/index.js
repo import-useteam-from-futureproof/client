0379fb17fb47dd1ba26cef0111c6b6d1570c0561
Zak Gogi <zakgogi1@gmail.com>
2021-08-19 22:40:53 +0100

    Add logic to only render canvas when every user finished quiz

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -25,46 +25,48 @@ export default function Results({ results, onQuizEnd }) {
 	}, [results]);
 
 	useEffect(() => {
-		const canvas = canvasRef.current;
-		const context = canvas.getContext('2d');
-		let scores = results.map((result) => result.score);
-		let players = results.map((result) => result.username);
-		var myChart = new Chart(context, {
-			type: 'bar',
-			data: {
-				labels: players,
-				datasets: [
-					{
-						label: '# of Points',
-						data: scores,
-						backgroundColor: [
-							'rgba(255, 99, 132, 0.2)',
-							'rgba(54, 162, 235, 0.2)',
-							'rgba(255, 206, 86, 0.2)',
-							'rgba(75, 192, 192, 0.2)',
-							'rgba(153, 102, 255, 0.2)',
-							'rgba(255, 159, 64, 0.2)',
-						],
-						borderColor: [
-							'rgba(255, 99, 132, 1)',
-							'rgba(54, 162, 235, 1)',
-							'rgba(255, 206, 86, 1)',
-							'rgba(75, 192, 192, 1)',
-							'rgba(153, 102, 255, 1)',
-							'rgba(255, 159, 64, 1)',
-						],
-						borderWidth: 1,
-					},
-				],
-			},
-			options: {
-				scales: {
-					y: {
-						beginAtZero: true,
+		if (!allUsersComplete) {
+			const canvas = canvasRef.current;
+			const context = canvas.getContext('2d');
+			let scores = results.map((result) => result.score);
+			let players = results.map((result) => result.username);
+			var myChart = new Chart(context, {
+				type: 'bar',
+				data: {
+					labels: players,
+					datasets: [
+						{
+							label: '# of Points',
+							data: scores,
+							backgroundColor: [
+								'rgba(255, 99, 132, 0.2)',
+								'rgba(54, 162, 235, 0.2)',
+								'rgba(255, 206, 86, 0.2)',
+								'rgba(75, 192, 192, 0.2)',
+								'rgba(153, 102, 255, 0.2)',
+								'rgba(255, 159, 64, 0.2)',
+							],
+							borderColor: [
+								'rgba(255, 99, 132, 1)',
+								'rgba(54, 162, 235, 1)',
+								'rgba(255, 206, 86, 1)',
+								'rgba(75, 192, 192, 1)',
+								'rgba(153, 102, 255, 1)',
+								'rgba(255, 159, 64, 1)',
+							],
+							borderWidth: 1,
+						},
+					],
+				},
+				options: {
+					scales: {
+						y: {
+							beginAtZero: true,
+						},
 					},
 				},
-			},
-		});
+			});
+		}
 	}, [allUsersComplete]);
 
 	const renderResults = () => {
