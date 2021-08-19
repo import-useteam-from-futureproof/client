baf19a3bd2d8ada7c8ac0b42865dc72ef95d753b
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-08-19 17:59:03 +0100

    fix

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,7 +5,11 @@ import test from '../../assets/logo.png';
 const Header = () => {
 	return (
 		<section className={styles.headerContainer} aria-label="header container">
-			<img aria-label="site logo" className={styles.headerImage} src={test}></img>
+			<img
+				aria-label="site logo"
+				className={styles.headerImage}
+				src={'../../assets/logo.png'}
+			></img>
 		</section>
 	);
 };
