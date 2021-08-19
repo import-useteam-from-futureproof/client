87f2409e3320aa01d3428a2f82fcf23a66b2e7fd
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-08-19 18:10:56 +0100

    last

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -38,18 +38,19 @@ const config = {
 		new FaviconsWebpackPlugin(path.resolve(PUBLIC_DIRECTORY, 'logo.png')),
 	],
 	module: {
+		// helpers we want webpack to use
 		rules: [
-			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+			// specific instructions for each helper
+			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }, // transpile JavaScript files
 			{
 				test: /\.css$/,
 				use: ['style-loader', 'css-loader'],
-			},
+			}, // transpile css files
 			{
-				test: /\.(jpe?g|png|gif|svg)$/,
-				use: ['url-loader'],
-			},
+				test: /\.(png|svg|jpg|gif|pdf)$/,
+				use: ['file-loader'],
+			}, // transpile image files
 		],
 	},
 };
-
 module.exports = config;
